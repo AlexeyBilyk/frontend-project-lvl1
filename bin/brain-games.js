e010b20ce932ef5df26d04babd82d041c6b35977
Alexey Bilyk <maverick4730@gmail.com>
2022-12-22 22:06:18 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import sayHello from '../src/cli.js';
 
-console.log('node bin/brain-games.js');
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name + '!');
+sayHello();
