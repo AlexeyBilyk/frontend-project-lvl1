8384178f77b73c79f62bb293969a042e0b525914
Alexey Bilyk <maverick4730@gmail.com>
2022-12-26 20:58:22 +0300

    as

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
-const sayHello = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
+import sayHello from '../src/index.js';
 
 export const num = () => {
   const number = (Math.round(Math.random() * 100));
