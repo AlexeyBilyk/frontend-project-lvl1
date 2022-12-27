ffd93a0e26a038ddd4852b548b76e7ea238eacc6
Alexey Bilyk <maverick4730@gmail.com>
2022-12-27 21:59:49 +0300

    upload brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import {sayHello, rndNum, getAnswer, wrongAnswer} from '../src/index.js';
+
+const getCorrectGCD = (firstNumber, secondNumber) => {
+    if (!secondNumber){
+        return firstNumber;
+    }
+    return getCorrectGCD(secondNumber, firstNumber % secondNumber);
+}
+const game = () => {
+  const name = sayHello();
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 0; i < 3; i += 1) {
+    const firstNumber = rndNum();
+    const secondNumber = rndNum();
+    const question = `${firstNumber} ${secondNumber}`;
+    const answer = getAnswer(question);
+    const correctAnswer = getCorrectGCD(firstNumber, secondNumber);
+    
+    if (correctAnswer == answer) {
+      console.log('Correct!');
+    } else {
+      wrongAnswer(name, answer, correctAnswer);
+      break;
+    }
+    if (i === 2) {
+      console.log(`Congratulations, ${name}!`);
+    }
+  }
+};
+game();
