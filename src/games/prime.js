9aa85a69d1fb58e08ebc2fe871bb546cf48b699a
Alexey Bilyk <maverick4730@gmail.com>
2022-12-29 22:58:13 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,7 +7,7 @@ export const game = () => {
   const name = sayHello();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
-    const number = rndNum(1, 1);
+    const number = rndNum(2, 100);
     const answer = getAnswer(number);
     let corrAnswer = '';
     let wrongAnsw = '';
